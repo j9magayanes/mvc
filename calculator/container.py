606c45cb2bebefeedcc4d344b34c1bfd233e0bf8
j9magayanes <100691918+j9magayanes@users.noreply.github.com>
2022-09-17 23:04:37 +0200

    Add files via upload

---
+++ calculator/container.py
@@ -0,0 +1,30 @@
+from dependency_injector import containers, providers
+from dependency_injector.wiring import Provide, inject
+
+
+class Container(containers.DeclarativeContainer):
+
+    config = providers.Configuration()
+
+    api_client = providers.Singleton(
+
+        api_key=config.api_key,
+        timeout=config.timeout,
+    )
+
+    service = providers.Factory(
+        api_client=api_client,
+    )
+
+
+@inject
+def main(service:  Provide[Container.service]) -> None:
+    ...
+
+
+if __name__ == "__main__":
+    container = Container()
+    container.config.api_key.from_env("API_KEY", required=True)
+    container.config.timeout.from_env("TIMEOUT", as_=int, default=5)
+    container.wire(modules=[__name__])
+
